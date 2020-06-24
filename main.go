a1043a7c64e4c99caf796f130425bae187a369da
gkalinkov <georgekalinkov@gmail.com>
2020-06-24 09:40:18 +0300

    Fix run of main.go

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	contextdb "github.com/gecapo/rest-api-golang/database"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	contextdb.ConnectDataBase()
+
+	r.Run()
 }
